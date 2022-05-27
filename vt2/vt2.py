0e0832bff875857c63d2754d7b0e894729e83b63
Hannamari Heiniluoma <hannamari.h.heiniluoma@jyu.fi>
2022-05-27 12:16:05 +0300

    Add documenting comments and remove unnecessary commented-out sections

--- vt2/vt2.py
+++ vt2/vt2.py
@@ -1,17 +1,15 @@
 from flask import Flask, Response, request, render_template
 from flask_wtf.csrf import CSRFProtect
 from polyglot import PolyglotForm
-from wtforms import IntegerField, StringField, validators, IntegerField, ValidationError
+from wtforms import IntegerField, StringField, validators, IntegerField
 app = Flask(__name__)
 
 csrf = CSRFProtect(app)
 app.secret_key = 'a"\xf9$T\x88\xefT8[\xf1\xc4Y-r@\t\xec!5d\xf9\xcc\xa2\xaa'
 
-
-
 @app.route('/', methods=["GET", "POST"])
 def chess():
-
+    #luodaan lomake
     class ChessForm(PolyglotForm):
         x = IntegerField('Laudan koko', validators=[validators.InputRequired(message="Syöttämäsi arvo ei kelpaa"), validators.NumberRange(min=8,max=16, message="Syöttämäsi arvo ei kelpaa")])
         pelaaja1 = StringField('Pelaaja 1', validators=[validators.InputRequired(message="Syötä pelaajan nimi"), validators.Length(min=1, message="Syötä pelaajan nimi")])
@@ -19,10 +17,9 @@ def chess():
 
     form = ChessForm()
 
+    #poimitaan pelaajien nimet ja laudan koko lomakkeelta
     try:
         pelaaja1 = request.values.get("pelaaja1")
-        #if not pelaaja1:
-        #    pelaaja1 = request.values.get("prev_p1")
         if not pelaaja1:
             pelaaja1 = "Pelaaja 1"
     except:
@@ -30,8 +27,6 @@ def chess():
 
     try:
         pelaaja2 = request.values.get("pelaaja2")
-        #if not pelaaja2:
-        #    pelaaja2 = request.values.get("prev_p2")
         if not pelaaja2:
             pelaaja2 = "Pelaaja 2"
     except:
@@ -39,13 +34,12 @@ def chess():
 
     try:
         x = int(request.values.get("x"))
-        #if not x:
-        #    x = int(request.values.get("prev_x"))
         if x < 8 or x > 16:
             x = 8
     except:
         x = 8
 
+    #validoidaan lomakekenttien syötteet
     if request.method == 'POST':
         form.validate()
 
