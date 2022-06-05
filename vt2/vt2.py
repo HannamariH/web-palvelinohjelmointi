000682ddcf1d454a2c7dcc4ffc9f2f0f8be8041c
Hannamari Heiniluoma <hannamari.h.heiniluoma@jyu.fi>
2022-06-05 17:40:29 +0300

    Reload board when new game is created

--- vt2/vt2.py
+++ vt2/vt2.py
@@ -9,10 +9,11 @@ app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.secret_key = '"\xf9$T\x88\xefT8[\xf1\xc4Y-r@\t\xec!5d\xf9\xcc\xa2\xaa'
 
+
 @app.route('/', methods=["GET", "POST"])
 def chess():
 
-    #haetaan asetukset ja otetaan ne talteen
+    # haetaan asetukset ja otetaan ne talteen
     with urllib.request.urlopen("https://europe-west1-ties4080.cloudfunctions.net/vt2_taso1") as response:
         conf = json.load(response)
 
@@ -21,15 +22,18 @@ def chess():
     min = conf["min"]
     max = conf["max"]
 
-    #luodaan lomake
+    # luodaan lomake
     class ChessForm(PolyglotForm):
-        x = IntegerField('Laudan koko', validators=[validators.InputRequired(message="Syöttämäsi arvo ei kelpaa"), validators.NumberRange(min=min,max=max, message="Syöttämäsi arvo ei kelpaa")])
-        pelaaja1 = StringField('Pelaaja 1', validators=[validators.InputRequired(message="Syötä pelaajan nimi"), validators.Length(min=1, message="Syötä pelaajan nimi")])
-        pelaaja2 = StringField("Pelaaja 2", validators=[validators.InputRequired(message="Syötä pelaajan nimi"), validators.Length(min=1, message="Syötä pelaajan nimi")])
+        x = IntegerField('Laudan koko', validators=[validators.InputRequired(
+            message="Syöttämäsi arvo ei kelpaa"), validators.NumberRange(min=min, max=max, message="Syöttämäsi arvo ei kelpaa")])
+        pelaaja1 = StringField('Pelaaja 1', validators=[validators.InputRequired(
+            message="Syötä pelaajan nimi"), validators.Length(min=1, message="Syötä pelaajan nimi")])
+        pelaaja2 = StringField("Pelaaja 2", validators=[validators.InputRequired(
+            message="Syötä pelaajan nimi"), validators.Length(min=1, message="Syötä pelaajan nimi")])
 
     form = ChessForm()
 
-    #poimitaan pelaajien nimet ja laudan koko lomakkeelta
+    # poimitaan pelaajien nimet ja laudan koko lomakkeelta
     try:
         pelaaja1 = request.values.get("pelaaja1")
         if not pelaaja1:
@@ -55,22 +59,21 @@ def chess():
         clicked = request.values.get("clicked")
     except:
         clicked = None
-    print("clicked", clicked)      
 
     def create_pieces(x, direction):
         pieces = {}
         if direction == "top-to-bottom":
-            for i in range (1, x+1):
+            for i in range(1, x+1):
                 pieces[i] = [i]
         elif direction == "bottom-to-top":
-            for i in range (1, x+1):
+            for i in range(1, x+1):
                 pieces[i] = [x+1-i]
         else:
             return pieces
         return pieces
 
-    #poistetaan klikattu pelinappula laudalta
-    def remove_clicked(pieces, clicked):   
+    # poistetaan klikattu pelinappula laudalta
+    def remove_clicked(pieces, clicked):
         if not clicked:
             return pieces
         key = str(clicked.split(":")[0])
@@ -78,15 +81,23 @@ def chess():
         pieces[key].remove(int(value))
         return pieces
 
-    try:
-        pieces = json.loads(request.values.get("pieces"))
-        pieces = remove_clicked(pieces, clicked)
-    except:
-        #pieces luodaan annetun diagonaalisuunnan mukaan
+    #TODO: tää ei nyt ihan toimi, pitää miettiä, millä ehdoilla luodaan alusta ja milloin haetaan lomakkeelta
+    # alusta siis silloin, jos ei ole pelaajan antamia arvoja TAI jos ei ole vielä klikattu mitään
+    #JOS KÄYTETÄÄN GET-METODIA, NAPPULAN KLIKKAUS PIENENTÄÄ PELILAUDAN MINIMIKOKOON...
+    #POST-METODILLA TOIMII TÄLLÄ HETKELLÄ NÄTISTI
+    #TODO: mutta pitäisi ilmeisesti olla GET-metodi
+    if (not pelaaja1 and not pelaaja1 and not x) or not clicked:
         pieces = create_pieces(x, balls_direction)
+    else:
+        try:
+            pieces = json.loads(request.values.get("pieces"))
+            pieces = remove_clicked(pieces, clicked)
+        except:
+            # pieces luodaan annetun diagonaalisuunnan mukaan
+            pieces = create_pieces(x, balls_direction)
 
-    #validoidaan lomakekenttien syötteet
+    # validoidaan lomakekenttien syötteet
     if request.method == 'POST':
-        form.validate() 
+        form.validate()
 
-    return Response(render_template("pohja.xhtml", form=form, pelaaja1=pelaaja1, pelaaja2=pelaaja2, x=x, first=first, pieces=pieces, pieces_json= json.dumps(pieces)), mimetype="application/xhtml+xml;charset=UTF-8") 
+    return Response(render_template("pohja.xhtml", form=form, pelaaja1=pelaaja1, pelaaja2=pelaaja2, x=x, first=first, pieces=pieces, pieces_json=json.dumps(pieces)), mimetype="application/xhtml+xml;charset=UTF-8")
