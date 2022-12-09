f83f017c39b91232dd59487d14f0b065d4598272
Hannamari Heiniluoma <hannamari.h.heiniluoma@jyu.fi>
2022-12-09 16:39:55 +0200

    /modify aluillaan

--- vt3/vt3.py
+++ vt3/vt3.py
@@ -6,6 +6,8 @@ from functools import wraps
 import mysql.connector
 import mysql.connector.pooling
 from mysql.connector import errorcode
+#from wtforms import IntegerField, StringField, validators, IntegerField
+#from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -42,6 +44,7 @@ def auth(f):
 def signin():
 
     try:          
+        session.clear()
         # haetaan kilpailut valikkoon
         sql = """SELECT kisanimi, alkuaika FROM kilpailut"""
         cur = con.cursor()
@@ -75,7 +78,7 @@ def signin():
         cur.execute(sql, (username, race_name, race_year+"%"))        
         team = cur.fetchall()
         if team == []:
-            session.pop("loggedin", None)
+            session.clear()
             klikattu = request.form.get("kirjaudu", "")           
             if klikattu != "":
                 login_error = True        
@@ -86,12 +89,17 @@ def signin():
             m.update(password.encode("UTF-8"))
             if m.hexdigest() == team[0][1]:
                 session['loggedin'] = "ok"
-                return redirect(url_for('team_list', race_name=race_name, race_year=race_year))
+                session["team_name"] = team[0][0]
+                session["start_time"] = team[0][4]
+                session["race_name"] = race_name
+                session["race_year"] = race_year
+                print(session)
+                return redirect(url_for('team_list'))
             # jos ei ollut oikea salasana, pysytään kirjautumissivulla
-            session.pop("loggedin", None)
+            session.clear()
             return render_template('login.xhtml', races=races, login_error=login_error)
         except:
-            session.pop("loggedin", None)
+            session.clear()
             return render_template('login.xhtml', races=races, login_error=login_error)
     except:
         #TODO pass????
@@ -104,8 +112,9 @@ def signin():
 @app.route('/teams', methods=['GET'])
 @auth
 def team_list():
-    race_name=request.args.get("race_name")
-    race_year=request.args.get("race_year")
+
+    race_name = session["race_name"]
+    race_year = session["race_year"]
 
     sql = """SELECT s.sarjanimi, j.joukkuenimi, j.jasenet FROM kilpailut k, joukkueet j, sarjat s 
         WHERE k.alkuaika LIKE %s AND k.kisanimi LIKE %s
@@ -124,4 +133,29 @@ def team_list():
         team_list[2] = members_array
         teams_list.append(team_list)
 
-    return render_template('teamslist.xhtml', race_name=race_name, race_year=race_year, teams=teams_list)
+    return render_template('teamslist.xhtml', race_name=race_name, race_year=race_year, teams=teams_list, start_time=session["start_time"], team_name=session["team_name"])
+
+@app.route('/modify', methods=['GET'])
+@auth
+def modify_team():
+    #haetaan ko. kisan sarjat radiobuttoneihin
+    sql = """SELECT s.sarjanimi FROM sarjat s 
+            WHERE s.kilpailu IN (
+                SELECT k.id FROM kilpailut k 
+                WHERE k.kisanimi LIKE %s AND k.alkuaika LIKE %s) 
+            ORDER BY s.sarjanimi;"""
+    cur = con.cursor()
+    cur.execute(sql, (session["race_name"], session["race_year"]+"%"))        
+    sarjat = cur.fetchall()
+
+    #haetaan joukkueen tiedot valmiiksi lomakkeelle
+    sql = """SELECT j.jasenet, s.sarjanimi FROM joukkueet j, sarjat s 
+            WHERE j.sarja = s.id AND j.joukkuenimi LIKE %s;"""
+    cur = con.cursor()
+    cur.execute(sql, (session["team_name"],))        
+    team = cur.fetchall()
+    sarja = team[0][1]
+    members = team[0][0]
+    members = members.replace("[","").replace("]","").replace('"',"")
+    members_array = [x.strip() for x in members.split(",")]
+    return render_template("modify.xhtml", sarjat=sarjat, sarja=sarja, members=members_array, race_name=session["race_name"], race_year=session["race_year"], start_time=session["start_time"], team_name=session["team_name"])
